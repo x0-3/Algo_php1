b64375e0313b99286d47637bea15f6c8606ebc6e
x0-3 <82101286+x0-3@users.noreply.github.com>
2022-12-16 11:42:58 +0100

    Add files via upload

--- exo15.php
+++ exo15.php
@@ -1,25 +1,48 @@
 <?php
 
-// Créer une classe Personne (nom, prénom et date de naissance).
+// Créer une classe Personne (nom, prénom et $birthday de naissance).
 // Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.
 
 
 class Personne
 {
-    public $personne;
+    public $nom;
+    public $prenom;
+    public $birthday;
+    public $date;
+    public $dif;
 
-    function set_name($personne){
-        $this->name = $personne;
-        $this->name = $personne;
+
+    function __construct($nom, $prenom, $birthday){
+        $this->name =$nom;
+        $this->prenom =$prenom;
+        $this->birthday =$birthday;
     }
-
+    function get_name(){
+        return $this->name;
+    }
+    function get_prenom(){
+        return $this->prenom;
+    }
+    function get_birthday(){
+        return $this->birthday;
+    }
 
 }
 
-$p1= new Personne ();
-$p1-> set_name("Michel");
+$p1= new Personne("DUPONT ", " Michel ", " 1980-02-19 ");
+
+echo $p1->get_name();
+echo $p1->get_prenom();
+echo $p1->get_birthday() ." ans";
+
+echo "<br>";
 
 
-echo $p1->name;
+$p2= new Personne("DUCHEMIN ", " Alice ", " 1985-01-17 ");
+
+echo $p2->get_name();
+echo $p2->get_prenom();
+echo $p2->get_birthday() ."ans";
 
 ?>
