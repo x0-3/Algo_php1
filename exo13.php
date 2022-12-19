f63f27f5024aba19667357149ddc9e062275d12c
x0-3 <82101286+x0-3@users.noreply.github.com>
2022-12-19 13:41:05 +0100

    Add files via upload

--- exo13.php
+++ exo13.php
@@ -4,13 +4,13 @@
 // coefficient). Elle devra être affichée avec 2 décimales.
 
 $note = array ("10", "12", "8", "19", "3", "16", "11", "13", "9");
-$moy= array_sum($note)/count($note);// summary of the grades 
+$moy= array_sum($note)/count($note);// summary of the grades
 $formatted_moy = number_format((float)$moy,2,'.','');//2 decimals
 
 echo "Les notes obtenues par l’élève sont : ";
 //foreach to print the value from the table
 foreach($note as $value){
-   echo $value ." "; 
+   echo $value ." ";
 }
 echo "<br> Sa moyenne générale est donc de : $formatted_moy" ;
 
--- exo15.php
+++ exo15.php
@@ -1,48 +1,58 @@
 <?php
 
-// Créer une classe Personne (nom, prénom et $birthday de naissance).
+// Créer une classe Personne (nom, prénom et date de naissance).
 // Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.
 
 
 class Personne
 {
+
+    // Attribute
     public $nom;
     public $prenom;
     public $birthday;
-    public $date;
-    public $dif;
 
-
-    function __construct($nom, $prenom, $birthday){
+    function __construct($nom, $prenom, $birthday,){
         $this->name =$nom;
         $this->prenom =$prenom;
-        $this->birthday =$birthday;
+        $this->birthday = $birthday;
     }
+
     function get_name(){
         return $this->name;
     }
+
     function get_prenom(){
         return $this->prenom;
     }
+
     function get_birthday(){
         return $this->birthday;
     }
 
+    function calculerAge(){
+        $date = new Datetime(); //current date
+        $anniv = new DateTime($this-> get_birthday());
+        $dif = $anniv->diff($date)->format("%Y ans");
+    
+        return $dif;
+    }
+
 }
 
-$p1= new Personne("DUPONT ", " Michel ", " 1980-02-19 ");
 
-echo $p1->get_name();
+$p1= new Personne("DUPONT ", " Michel ", " 1980-02-19 ",);
+
 echo $p1->get_prenom();
-echo $p1->get_birthday() ." ans";
+echo $p1->get_name();
+echo $p1->calculerAge();
 
 echo "<br>";
 
-
 $p2= new Personne("DUCHEMIN ", " Alice ", " 1985-01-17 ");
 
-echo $p2->get_name();
 echo $p2->get_prenom();
-echo $p2->get_birthday() ."ans";
+echo $p2->get_name();
+echo $p2->calculerAge();
 
 ?>
